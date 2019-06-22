71410f95161585f2fa4b66b4a61d829070ca44c5
jtwray <tuckerwray84@gmail.com>
2019-06-21 22:13:27 -0400

    Fetches correct movie based on match.params.id prop from App.js Route render ...props

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
+import MovieCard from './MovieCard';
 import React, { Component } from 'react';
 import axios from 'axios';
-
 export default class Movie extends Component {
   constructor(props) {
     super(props);
@@ -10,9 +10,10 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
+    
+
     const id = 1;
-    this.fetchMovie(id);
+    this.fetchMovie(this.props.match.params.id)
   }
 
   fetchMovie = id => {
@@ -41,28 +42,9 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
+return(
 
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className="save-button">Save</div>
-      </div>
-    );
-  }
+  <MovieCard movie={this.state.movie} /> 
+  )
+}
 }
